2b69784fc1b77daa7bfc3b5840b377f3355234f7
Ashwani <Ashwani@192.168.0.22>
2020-03-08 20:25:10 -0500

    First Commit

---
+++ Swagger-Docket-app/src/main/java/com/Capitals/Service/SwaggerDocketAppApplication.java
@@ -0,0 +1,13 @@
+package com.Capitals.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerDocketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerDocketAppApplication.class, args);
+	}
+
+}
---
+++ Swagger-Docket-app/src/test/java/com/Capitals/Service/SwaggerDocketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Capitals.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerDocketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
